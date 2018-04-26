35464f3ed8e083bf9529c12897b98876595434f6
Laharnar <marko.laharnar@gmail.com>
2018-04-26 23:09:02 +0200

    reflection functions

--- GamesFramework_strategy_ver1/Assets/Engine/Reflection/ReflectionHelper.cs
+++ GamesFramework_strategy_ver1/Assets/Engine/Reflection/ReflectionHelper.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Reflection;
 using UnityEngine;
 public static class ReflectionHelper {
+
     public static List<string> GetAllNames(string nspace) {
         /*var q = AppDomain.CurrentDomain.GetAssemblies()
                        .SelectMany(t => t.GetTypes())
@@ -33,3 +34,28 @@ public static class ReflectionHelper {
         return list;
     }
 }
+/*
+ if (GUILayout.Button("Try init")) {
+            try { 
+                Type typ = typeof(PointTarget).Assembly.GetType(SOClass.allClasses[source.selected]);
+                Debug.Log(typ);
+                source.element =(PointTarget) Activator.CreateInstance(typ);
+            } catch (Exception e) {
+                Debug.Log(e);
+            }
+        }
+        if (source.element != null) {
+            List<string> list = new List<string>();
+            EditorGUILayout.LabelField("Methods:");
+            EditorGUI.indentLevel++;
+            (from t in source.element.GetType().GetMethods()
+                select t.Name).ToList().ForEach(t => EditorGUILayout.LabelField(t));
+            EditorGUI.indentLevel--;
+
+            EditorGUILayout.LabelField("Values:"+ source.element.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic).Length);
+            EditorGUI.indentLevel++;
+            (from t in source.element.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic)
+             select t).ToList().ForEach(t => t.SetValue(source.element, ShowValue(source.element, t)));
+            EditorGUI.indentLevel--;
+        
+*/
