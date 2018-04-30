54b4134b5f3a937b2d2252147d430be111b855a4
Laharnar <marko.laharnar@gmail.com>
2018-04-30 13:03:39 +0200

    Standard nested pref solution 1: build object out of empty GO targets.

---
+++ GamesFramework_strategy_ver1/Assets/Engine/NestedPrefabs/NestedPrefabLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+/// <summary>
+/// Standard nested pref solution: build object out of empty GO targets.
+/// How to use: In play mode, create the object. Then create empty child GO's on positions where nested pref should be 
+/// instantiated. Then add this script to it and link it to pref.
+/// </summary>
+public class NestedPrefabLoader:MonoBehaviour {
+    public Transform prefTarget;
+    private void Awake() {
+        Instantiate(prefTarget, transform.position, transform.rotation, transform.parent);
+    }
+}
