46af0a53e75d24013e640be44d047daf710f7dfd
Laharnar <marko.laharnar@gmail.com>
2018-05-10 23:51:00 +0200

    waves and level manager

---
+++ GamesFramework_strategy_ver1/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public float speed = 1f;
+
+    public List<WaveSO> wavePrefs=new List<WaveSO>();
+    List<Wave> waves=new List<Wave>();
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(LevelLoop());	
+	}
+
+    private IEnumerator LevelLoop() {
+        // spawn waves when all enemies from current wave, whenlast wave is cleared
+        while (wavePrefs.Count>0) {
+            waves.Add(Wave.InitWave(wavePrefs[0]));
+            while (waves[0].units.Count>0) {
+                yield return null;
+            }
+            wavePrefs.RemoveAt(0);
+        }
+        yield return null;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // move level left
+        transform.Translate(Vector3.left*Time.deltaTime*speed);
+	}
+
+}
